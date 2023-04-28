55fb58fe5f436b60df6b82a73b5912b5b24b8b8b
BrianV87 <Brian.v87@hotmail.fr>
2023-04-28 13:59:23 +0200

    Mise en place de la partie temoignage sur l'admin

--- src/Model/TestimonialManager.php
+++ src/Model/TestimonialManager.php
@@ -7,4 +7,23 @@ use PDO;
 class TestimonialManager extends AbstractManager
 {
     public const TABLE = 'testimonial';
+
+    public function insert(array $testimonial): int
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (testimonial)
+        VALUES (:testimonial)");
+        $statement->bindValue('testimonial', $testimonial['testimonial'], PDO::PARAM_STR);
+
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
+    public function update(array $testimonial): bool
+    {
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE .
+            " SET testimonial = :testimonial WHERE id=:id");
+        $statement->bindValue('id', $testimonial['id'], PDO::PARAM_INT);
+        $statement->bindValue('testimonial', $testimonial['testimonial'], PDO::PARAM_STR);
+
+        return $statement->execute();
+    }
 }
